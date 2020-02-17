2284d9cda1058dcba9317dbc86db3375e6125e44
Steve Lee <qwersteve07@gmail.com>
2020-02-18 01:24:53 +0800

    testing

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,11 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
     let value = input.value;
     name.textContent = 'rendering'
-    fetch(`http://localhost:3000/weather?addr=${value}`).then(response=>{
+    fetch(`/weather?addr=${value}`).then(response=>{
     response.json().then(data=>{
-        console.log(data)
         if(data.error){
             name.innerText = data.error;
         }else{
-            console.log(data)
             name.innerText = data.name;
             temperature.innerText = data.temperature;
         }
